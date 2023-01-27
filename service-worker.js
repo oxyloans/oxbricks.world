099974ed2c0ed3e366f8116841f439cb9c7f8eb9
SUBASH SURE <subbu@oxyloans.com>
2023-01-27 18:51:12 +0530

    updates by build 12

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3fb38013fdef6ae35573bfcf07cc222a.js"
+  "/precache-manifest.7f95f572c3f5a85bd9abb672ba93585f.js"
 );
 
 self.addEventListener('message', (event) => {
